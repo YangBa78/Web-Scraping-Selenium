6f7b79a9aa3d1ef28a634894856838b3a00a41fb
Yang Ba <65861171+YangBa78@users.noreply.github.com>
2021-02-27 21:28:03 -0500

    Add files via upload

---
+++ dynamic.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+import pandas as pd
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+url = 'https://www.youtube.com/c/JohnWatsonRooney/videos?view=0&sort=p&flow=grid'
+driver.get(url)
+
+
+videos = driver.find_elements_by_class_name('style-scope ytd-grid-video-renderer')
+
+video_list = []
+
+for video in videos:
+    title = video.find_element_by_xpath('.//*[@id="video-title"]').text
+    views = video.find_element_by_xpath('.//*[@id="metadata-line"]/span[1]').text
+    when = video.find_element_by_xpath('.//*[@id="metadata-line"]/span[2]').text
+    vid_item = {
+        'title': title, 
+        'views': views,
+        'posted': when
+    }
+
+    video_list.append(vid_item)
+
+df = pd.DataFrame(video_list)
+print(df)
+
